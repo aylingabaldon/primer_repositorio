8638e281cb07479ce7dfb7df0079970193859db8
aylingabaldon <aylingabaldon@gmail.com>
2025-10-01 09:17:58 -0600

    Create Programa1.py

---
+++ Programa1.py
@@ -0,0 +1,6 @@
+# Este es un script de Python de prueba para el repositorio
+def saludo(nombre):
+  return "Hola "+nombre
+
+# Se invoca a la función saludo para saludar a alguien con un nombre en específico
+saludo("Aylin")
